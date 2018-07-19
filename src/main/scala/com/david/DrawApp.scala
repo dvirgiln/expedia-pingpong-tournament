7e69ab554f231214d84969c69798d720faa2c443
Sean Jackson <sejackson@LONC02TNCYBG8WN.sea.corp.expecn.com>
2018-07-19 08:46:09 +0100

    Added the fourth group

--- src/main/scala/com/david/DrawApp.scala
+++ src/main/scala/com/david/DrawApp.scala
@@ -4,12 +4,13 @@ object DrawApp extends App {
   val favourites = scala.io.Source.fromFile("src/main/resources/groupFavourites.txt").getLines().toList
   val group2 = scala.io.Source.fromFile("src/main/resources/secondGroup.txt").getLines().toList
   val group3 = scala.io.Source.fromFile("src/main/resources/thirthGroup.txt").getLines().toList
-  val group3Full= (1 to (group3.length % 8)).foldLeft(List[String]())((acum, _) => "" :: acum) ::: group3
+  val group4 = scala.io.Source.fromFile("src/main/resources/fourthGroup.txt").getLines().toList
+  val group4full= (1 to (group4.length % 8)).foldLeft(List[String]())((acum, _) => "" :: acum) ::: group4
 
-  val group3Shuffled= scala.util.Random.shuffle(group3Full)
-  val group3Splited=(1 to group3Shuffled.length/8).foldLeft((List[List[String]](),group3Shuffled)){case ((result, rest), _) => (rest.take(8) :: result, rest.drop(8))}
+  val group4Shuffled= scala.util.Random.shuffle(group4full)
+  val group4Splited=(1 to group4Shuffled.length/8).foldLeft((List[List[String]](),group4Shuffled)){case ((result, rest), _) => (rest.take(8) :: result, rest.drop(8))}
 
-  val allGroups= scala.util.Random.shuffle(favourites) :: scala.util.Random.shuffle(group2) :: group3Splited._1
+  val allGroups= scala.util.Random.shuffle(favourites) :: scala.util.Random.shuffle(group3) :: scala.util.Random.shuffle(group2) :: group4Splited._1
 
   val results = allGroups.map(g => g.zipWithIndex).flatten.groupBy(_._2).map(_._2).toList.map(l => l.map(_._1).filter(!_.isEmpty))
   results.zipWithIndex.foreach{ case (group, i) =>
