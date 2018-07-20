16f544d6f0735ecb9bd9f626ad4b1b8cce8407f3
dvirgiln <dvirgil@expedia.com>
2018-07-20 09:50:43 +0100

    improvements

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 Repository that creates random groups for a tournament.
 
+##RULES
+
+* Group matches are played as the best of 5 sets of 11 points. 2 serves per player.
+* Classificatory matches are played as the best of 5 21 points. 5 serves per player.
+* There is not specific order to play your games. You can arrange with the other players when to play.
+
+
+### ProRound
+
     GROUP 1 1st vs GROUP 8 2nd                                                                                                         GROUP 5 1st vs GROUP 7 2nd
     ==========================                                                                                                         ==========================
                              ===                                                                                                    ===
@@ -22,3 +31,26 @@ Repository that creates random groups for a tournament.
                              ===                                                                                                    ===
     GROUP 4 1st vs GROUP 3 2nd                                                                                                         GROUP 8 1st vs GROUP 4 2nd
     ==========================                                                                                                          ==========================
+
+###Improvers Round
+
+    GROUP 1 3th vs GROUP 8 4th                                                                                                         GROUP 5 3th vs GROUP 7 4th
+    --------------------------                                                                                                         --------------------------
+                             ---                                                                                                    ---
+                                ---                                                                                              ---
+                                   ---   QUARTER FINAL                                                         QUARTER FINAL  ---
+                                ---                   ---                                                  ---                   ---
+                             ---                        ---                                              ---                        ---
+    GROUP 7 3th vs GROUP 5 4th                            ---                                         ---                              GROUP 6 3th vs GROUP 2 4th
+    --------------------------                              ---                                    ---                                 --------------------------
+                                                               ---                              ---
+                                                                   SEMIFINAL -- FINAL -- SEMIFINAL
+    GROUP 2 3th vs GROUP 6 4th                                  ---                               ---                                  GROUP 3 3th vs GROUP 1 4th
+    --------------------------                              ---                                      ---                               --------------------------
+                             ---                         ---                                            ---                         ---
+                                ---                   ---                                                  ---                   ---
+                                   ---   QUARTER FINAL                                                         QUARTER FINAL  ---
+                                ---                                                                                              ---
+                             ---                                                                                                    ---
+    GROUP 4 3th vs GROUP 3 4th                                                                                                         GROUP 8 3th vs GROUP 4 4th
+    --------------------------                                                                                                          --------------------------
--- src/main/scala/com/david/DrawApp.scala
+++ src/main/scala/com/david/DrawApp.scala
@@ -13,33 +13,42 @@ object DrawApp extends App {
   val allGroups= scala.util.Random.shuffle(favourites) :: scala.util.Random.shuffle(group3) :: scala.util.Random.shuffle(group2) :: group4Splited._1
 
   val results = allGroups.map(g => g.zipWithIndex).flatten.groupBy(_._2).map(_._2).toList.map(l => l.map(_._1).filter(!_.isEmpty))
+  val groupByIndex = (1 to 8) zip ('A' to 'H')
   results.zipWithIndex.foreach{ case (group, i) =>
-    println(s"GROUP ${i+1}")
+    println(s"GROUP ${groupByIndex(i)._2 }")
     println("_______________")
     println(s"\t${group.mkString("\n\t")}")
   }
   println()
-  println("GROUP 1 1st vs GROUP 8 2nd                                                                                                         GROUP 5 1st vs GROUP 7 2nd")
-  println("--------------------------                                                                                                         --------------------------")
-  println("                         ---                                                                                                    ---")
-  println("                            ---                                                                                              ---")
-  println("                               ---   QUARTER FINAL                                                         QUARTER FINAL  ---")
-  println("                            ---                   ---                                                  ---                   ---")
-  println("                         ---                        ---                                              ---                        ---")
-  println("GROUP 7 1st vs GROUP 5 2nd                            ---                                         ---                              GROUP 6 1st vs GROUP 2 2nd")
-  println("--------------------------                              ---                                    ---                                 --------------------------")
-  println("                                                           ---                              ---")
-  println("                                                               SEMIFINAL -- FINAL -- SEMIFINAL")
-  println("GROUP 2 1st vs GROUP 6 2nd                                  ---                               ---                                  GROUP 3 1st vs GROUP 1 2nd")
-  println("--------------------------                              ---                                      ---                               --------------------------")
-  println("                         ---                         ---                                            ---                         ---")
-  println("                            ---                   ---                                                  ---                   ---")
-  println("                               ---   QUARTER FINAL                                                         QUARTER FINAL  ---")
-  println("                            ---                                                                                              ---")
-  println("                         ---                                                                                                    ---")
-  println("GROUP 4 1st vs GROUP 3 2nd                                                                                                         GROUP 8 1st vs GROUP 4 2nd")
-  println("--------------------------                                                                                                          --------------------------")
+  defineGroups(("1st", "2nd"))
+  println()
+  defineGroups(("3th", "4th"))
+
+  def defineGroups(pos: (String, String))={
+
+    println(s"GROUP 1 ${pos._1} vs GROUP 8 ${pos._2}                                                                                                         GROUP 5 ${pos._1} vs GROUP 7 ${pos._2}")
+    println("--------------------------                                                                                                         --------------------------")
+    println("                         ---                                                                                                    ---")
+    println("                            ---                                                                                              ---")
+    println("                               ---   QUARTER FINAL                                                         QUARTER FINAL  ---")
+    println("                            ---                   ---                                                  ---                   ---")
+    println("                         ---                        ---                                              ---                        ---")
+    println(s"GROUP 7 ${pos._1} vs GROUP 5 ${pos._2}                            ---                                         ---                              GROUP 6 ${pos._1} vs GROUP 2 ${pos._2}")
+    println("--------------------------                              ---                                    ---                                 --------------------------")
+    println("                                                           ---                              ---")
+    println("                                                               SEMIFINAL -- FINAL -- SEMIFINAL")
+    println(s"GROUP 2 ${pos._1} vs GROUP 6 ${pos._2}                                  ---                               ---                                  GROUP 3 ${pos._1} vs GROUP 1 ${pos._2}")
+    println("--------------------------                              ---                                      ---                               --------------------------")
+    println("                         ---                         ---                                            ---                         ---")
+    println("                            ---                   ---                                                  ---                   ---")
+    println("                               ---   QUARTER FINAL                                                         QUARTER FINAL  ---")
+    println("                            ---                                                                                              ---")
+    println("                         ---                                                                                                    ---")
+    println(s"GROUP 4 ${pos._1} vs GROUP 3 ${pos._2}                                                                                                         GROUP 8 ${pos._1} vs GROUP 4 ${pos._2}")
+    println("--------------------------                                                                                                          --------------------------")
 
 
+
+  }
 
 }
